82e8cd1a503fe2ecf8dff92a339516e13f9fc9de
Saifil Ali <sali@osv.com>
2020-10-12 13:15:10 -0500

    added 2 services to handle most of the things.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,18 +8,13 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test','https://cdn.healthykitchen101.com/uploads/2019/04/Gumbo-recipe-1.jpg'),
-    new Recipe('Another Test Recipe', 'This is simply an another test','https://cdn.healthykitchen101.com/uploads/2019/04/Gumbo-recipe-1.jpg'),
-  ];
 
-  constructor() { }
+  recipes: Recipe[];
+
+
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
-  }
-
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
+    this.recipes = this.recipeService.getRecipes();
   }
 }
